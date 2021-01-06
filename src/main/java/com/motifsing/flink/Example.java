5565899d85d592127ea408b52698e424ba1b88ce
yhw <1228422874@qq.com>
2021-01-06 18:28:17 +0800

    add wordcount of java

---
+++ src/main/java/com/motifsing/flink/Example.java
@@ -0,0 +1,54 @@
+package com.motifsing.flink;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.common.functions.FilterFunction;
+
+/**
+ * @ClassName Example
+ * @Description
+ * @Author Administrator
+ * @Date 2020/12/9 17:02
+ * @Version 1.0
+ **/
+public class Example {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Person> flintstones = env.fromElements(
+                new Person("Fred", 35),
+                new Person("Wilma", 35),
+                new Person("Pebbles", 2));
+
+        DataStream<Person> adults1 = flintstones.filter(new FilterFunction<Person>() {
+            @Override
+            public boolean filter(Person person) {
+                return person.age >= 18;
+            }
+        });
+        DataStream<Person> adults2 = flintstones.filter((FilterFunction<Person>) person -> person.age >= 18);
+
+        DataStream<Person> adults = flintstones.filter((person) -> person.age >= 18);
+
+        adults.print();
+
+        env.execute();
+    }
+
+    public static class Person {
+        public String name;
+        public Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": age " + this.age.toString();
+        }
+    }
+}
---
+++ src/main/java/com/motifsing/flink/JavaWindowWordCount.java
@@ -0,0 +1,59 @@
+package com.motifsing.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * @ClassName WindowWordCount
+ * @Description WindowWordCount统计
+ * @Author Administrator
+ * @Date 2020/12/23 14:53
+ * @Version 1.0
+ **/
+public class JavaWindowWordCount {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        for (String word: value.split(" ")) {
+                            out.collect(new Tuple2<>(word, 1));
+                        }
+                    }
+                })
+                .keyBy(value -> value.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+//        DataStream<Tuple2<String, Integer>> dataStream = env
+//                .socketTextStream("localhost", 9999)
+//                .flatMap(new Splitter())
+//                .keyBy(value -> value.f0)
+//                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+//                .sum(1);
+
+        dataStream.print();
+        env.execute("WindowWordCount");
+
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            for (String word: value.split(" ")) {
+                out.collect(new Tuple2<>(word, 1));
+            }
+        }
+    }
+}
