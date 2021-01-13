6abfe1c27f7da01808f43903c61cdc29b660ef4f
yhw <1228422874@qq.com>
2021-01-13 17:47:18 +0800

    add operator

---
+++ src/main/java/com/motifsing/flink/operator/CountryCodeKeyBy.java
@@ -0,0 +1,93 @@
+package com.motifsing.flink.operator;
+
+import com.motifsing.flink.source.FileCountryDictSourceFunction;
+import com.motifsing.flink.source.MyKafkaRecord;
+import com.motifsing.flink.source.MyKafkaRecordSchema;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.KeyedCoProcessFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+/**
+ * @ClassName CountryCodeKeyBy
+ * @Description
+ * @Author Motifsing
+ * @Date 2021/1/13 17:19
+ * @Version 1.0
+ **/
+public class CountryCodeKeyBy {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        String path = "/user/test/test.txt";
+        DataStreamSource<String> countryCodeSource = env.addSource(new FileCountryDictSourceFunction(path));
+
+        KeyedStream<Tuple2<String, String>, String> countryCodeKeyedStream = countryCodeSource.map(new MapFunction<String, Tuple2<String, String>>() {
+            @Override
+            public Tuple2<String, String> map(String value) throws Exception {
+                String[] s = value.split(" ");
+                return Tuple2.of(s[0], s[1] + "-" + s[2]);
+            }
+        }).keyBy(new KeySelector<Tuple2<String, String>, String>() {
+            @Override
+            public String getKey(Tuple2<String, String> value) throws Exception {
+                return value.f0;
+            }
+        });
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "172.23.36.74:9092");
+        properties.setProperty("group.id", "test");
+        properties.setProperty("flink.partition-discovery.interval-millis", "30000");
+
+        String topic = "test";
+
+        FlinkKafkaConsumer<MyKafkaRecord> kafkaConsumer = new FlinkKafkaConsumer<>(topic, new MyKafkaRecordSchema(), properties);
+        kafkaConsumer.setStartFromLatest();
+
+        DataStreamSource<MyKafkaRecord> kafkaInput = env.addSource(kafkaConsumer);
+
+        KeyedStream<MyKafkaRecord, String> kafkaKeyedStream = kafkaInput.keyBy(new KeySelector<MyKafkaRecord, String>() {
+            @Override
+            public String getKey(MyKafkaRecord value) throws Exception {
+                return value.getRecord();
+            }
+        });
+
+        ConnectedStreams<Tuple2<String, String>, MyKafkaRecord> connect = countryCodeKeyedStream.connect(kafkaKeyedStream);
+
+        SingleOutputStreamOperator<String> process = connect.process(new KeyedCoProcessFunction<String, Tuple2<String, String>, MyKafkaRecord, String>() {
+
+            HashMap<String, String> hashMap = new HashMap<>();
+
+            @Override
+            public void processElement1(Tuple2<String, String> value, Context ctx, Collector<String> out) throws Exception {
+                hashMap.put(value.f0, value.f1);
+                out.collect(value.toString());
+            }
+
+            @Override
+            public void processElement2(MyKafkaRecord value, Context ctx, Collector<String> out) throws Exception {
+                String countryName = hashMap.get(value.getRecord());
+                String outStr = countryName == null ? "no match" : countryName;
+                out.collect(value + ": " + outStr);
+            }
+        });
+
+        process.print();
+
+        env.execute();
+    }
+}
---
+++ src/main/java/com/motifsing/flink/operator/CountryCodeUnion.java
@@ -0,0 +1,70 @@
+package com.motifsing.flink.operator;
+
+import com.motifsing.flink.source.FileCountryDictSourceFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+/**
+ * @ClassName CountryCodeUnion
+ * @Description
+ * @Author Motifsing
+ * @Date 2021/1/13 15:52
+ * @Version 1.0
+ **/
+public class CountryCodeUnion {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        env.setParallelism(1);
+
+        String path = "/user/test/test.txt";
+        DataStreamSource<String> countryCodeSource = env.addSource(new FileCountryDictSourceFunction(path));
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "172.23.36.74:9092");
+        properties.setProperty("group.id", "test");
+        properties.setProperty("flink.partition-discovery.interval-millis", "30000");
+
+        String topic = "test";
+
+        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), properties);
+        kafkaConsumer.setStartFromLatest();
+
+        DataStreamSource<String> kafkaInput = env.addSource(kafkaConsumer);
+
+        DataStream<String> union = countryCodeSource.union(kafkaInput);
+
+        SingleOutputStreamOperator<String> process = union.process(new ProcessFunction<String, String>() {
+
+            HashMap<String, String> hashMap = new HashMap<>();
+
+            @Override
+            public void processElement(String value, Context ctx, Collector<String> out) throws Exception {
+                String[] s = value.split(" ");
+                if (s.length > 1) {
+                    hashMap.put(s[0], s[1] + "-" + s[2]);
+                    out.collect(value);
+                } else {
+                    String countryName = hashMap.get(value);
+                    String outStr = countryName == null ? "no match" : countryName;
+                    out.collect(outStr);
+                }
+            }
+        });
+
+        process.print();
+
+        env.execute();
+    }
+}
